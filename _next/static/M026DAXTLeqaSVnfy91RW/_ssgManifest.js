d15fbdc6c45f986d6432f723486dba825f0a558b
Dmitry Shibanov <dmishib@gmail.com>
2025-12-07 03:51:05 +0500

    Deploy: 2025-12-07 03:51:05

---
+++ _next/static/M026DAXTLeqaSVnfy91RW/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002Fapi\u002Fprojects\u002F[locale]\u002F[id]","\u002Fapi\u002Fprojects\u002F[locale]\u002Fpage\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/api/projects/[locale]/page/[page]/route-33fe9049f5afd015.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[917,947],{7684:(e,s,_)=>{Promise.resolve().then(_.bind(_,7088))}},e=>{e.O(0,[707,441,255,358],()=>e(e.s=7684)),_N_E=e.O()}]);
